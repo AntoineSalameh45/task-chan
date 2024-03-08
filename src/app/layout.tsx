dfe01036e9f39025d126d436a2c1eea7f9feb552
BiG A <antoine.salameh.45@gmail.com>
2024-03-09 00:35:41 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
+/* @jsxImportSource react */
+import React from 'react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/organisms/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Task-chan",
   description: "Task Manager next app",
+  keywords: ["task manager", "task", "tasks"],
 };
 
 export default function RootLayout({
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from 'react';
+import TaskCard, { Task } from './components/molecules/taskcard';
+import TaskForm from './components/molecules/taskform'; // Assuming you have a TaskForm component
 
-export default function Home() {
+const Home: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAddTask = (newTask: Task) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-          <h1
-            className="relative "
-          >
-            Yo!
-          </h1>
+        <div className="relative flex place-items-center ">
+          <TaskForm addTask={handleAddTask} />
+        </div>
+        <div className='flex gap-4 flex-wrap'>
+          {tasks.map(task => (
+            <TaskCard key={task.id} task={task} />
+          ))}
         </div>
       </main>
     </>
   );
-}
+};
+
+export default Home;
